d2d3de53c3e0a331766b41db4552eead4f069516
nihadaslanli <nihadsasl@code.edu.az>
2024-02-22 16:48:39 +0400

    Add project files.

---
+++ #hometask_Overloading_method/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+#region task1
+//int sum = 0;
+//int[] numbers = { 9,24,40,45,75, }; 
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > 10 && numbers[i] < 100)
+//    {
+//        sum += numbers[i];
+//        Console.WriteLine(sum);
+//    }
+//}
+
+//return sum;
+#endregion
+#region task2
+//string mevsim = FindSeason(7);
+//static string FindSeason(int ay)
+//{
+//    switch (ay)
+//    {
+//        case 3:
+//        case 4:
+//        case 5:
+//            return "Yaz";
+//        case 6:
+//        case 7:
+//        case 8:
+//            return "Yay";
+//        case 9:
+//        case 10:
+//        case 11:
+//            return "Payiz";
+//        case 12:
+//        case 1:
+//        case 2:
+//            return "Qish";
+//        default:
+//            return "Duzgun Ayi daxil edin";
+            
+
+//    }
+//}
+
+//Console.WriteLine("fesil: " + mevsim);
+
+#endregion
