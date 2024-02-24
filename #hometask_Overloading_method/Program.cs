9cdc974ae84003a4f5d65c35ded29dd791dfd29d
nihadaslanli <nihadsasl@code.edu.az>
2024-02-24 21:49:38 +0400

    feat:add file

--- #hometask_Overloading_method/Program.cs
+++ #hometask_Overloading_method/Program.cs
@@ -50,23 +50,32 @@
 
 #region task3
 
-Console.Write("Üç reqemli eded daxil edin: ");
-int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Üç reqemli eded daxil edin: ");
+//int number = Convert.ToInt32(Console.ReadLine());
 
 
-if (number < 1000 && number > 99)
-{
-    int tekliklermertebesi = number % 10;
-    int onluqlarmertebesi = (number / 10) % 10;
-    int yüzluklermertebesi = number / 100;
-    int cem = tekliklermertebesi * onluqlarmertebesi * yüzluklermertebesi;
-    Console.WriteLine(cem);
-}
+//if (number < 1000 && number > 99)
+//{
+//    int tekliklermertebesi = number % 10;
+//    int onluqlarmertebesi = (number / 10) % 10;
+//    int yüzluklermertebesi = number / 100;
+//    int cem = tekliklermertebesi * onluqlarmertebesi * yüzluklermertebesi;
+//    Console.WriteLine(cem);
+//}
 
 
 #endregion
 #region task4
 
-
+int[] vaule = { 23, 4, 56 };
+for (int i = 0; i < vaule.Length; i++)
+{
+    if (vaule[0] < vaule[1] && vaule[0] < vaule[2])
+    { Console.WriteLine(vaule[0]); break; }
+    else if (vaule[1] < vaule[2] && vaule[1] < vaule[0])
+    { Console.WriteLine(vaule[1]); break; }
+    else if (vaule[2] < vaule[0] && vaule[2] < vaule[1])
+    { Console.WriteLine(vaule[2]); break; }
+}
 
 #endregion
